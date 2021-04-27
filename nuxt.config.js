93079a102fa2e3296e63a460ac8460216fe676bd
stephenlaichaowen <stephenlaitest@gmail.com>
2021-04-27 13:46:25 +0800

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 export default {
   target: 'static',
@@ -19,7 +19,7 @@ export default {
 
   components: true,
 
-  buildModules: ['@nuxtjs/composition-api/module', '@nuxtjs/dotenv'],
+  // buildModules: ['@nuxtjs/composition-api/module', '@nuxtjs/dotenv'],
   buildModules: ['@nuxtjs/composition-api/module'],
 
   modules: ['@nuxtjs/pwa'],
