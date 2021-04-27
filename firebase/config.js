95711d916958e6171b1d58a0df257a7166461f36
stephenlaichaowen <stephenlaitest@gmail.com>
2021-04-27 14:18:32 +0800

    commit

--- firebase/config.js
+++ firebase/config.js
@@ -2,14 +2,6 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  // apiKey: process.env.API_KEY,
-  // authDomain: process.env.AUTH_DOMAIN,
-  // databaseURL: process.env.DATABASE_URL,
-  // projectId: process.env.PROJECT_ID,
-  // storageBucket: process.env.STORAGE_BUCKET,
-  // messagingSenderId: process.env.MESSAGING_SENDERID,
-  // appId: process.env.APP_ID,
-
   apiKey: 'AIzaSyBm9VXXj0pjKwkWrr4lJqsGrpBHkLUdRho',
   authDomain: 'udemy-course-nextjs.firebaseapp.com',
   databaseURL: 'https://udemy-course-nextjs-default-rtdb.firebaseio.com',
